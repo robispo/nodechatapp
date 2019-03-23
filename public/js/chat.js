a18d5cc5fdd3a059c034cceba865057b3c3a3daf
Rabel Obispo <rabelobispo@hotmail.com>
2019-03-23 18:39:11 -0400

    disabled buttons when you're sharing info

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,8 @@
 const socket = io();
-const eleUserName = document.getElementById('inpUserName');
-const eleMessage = document.getElementById('taMessage');
+const eleForm = document.getElementById('fChat');
+const eleMessage = document.getElementById('inpMessage');
+const eleSubmit = document.getElementById('btnSubmit');
+const eleSendLocation = document.getElementById('btnShareLocation');
 
 socket.on('Welcome', greetings => {
   console.log(greetings);
@@ -10,8 +12,11 @@ socket.on('receiveMessage', message => {
   console.log(message);
 });
 
-document.getElementById('fChat').addEventListener('submit', e => {
+eleForm.addEventListener('submit', e => {
   e.preventDefault();
+  eleForm.disabled = true;
+  eleSubmit.disabled = true;
+
   const form = e.target.elements;
 
   socket.emit(
@@ -21,6 +26,11 @@ document.getElementById('fChat').addEventListener('submit', e => {
       message: form.message.value
     },
     e => {
+      eleForm.disabled = false;
+      eleSubmit.disabled = false;
+      eleMessage.value = '';
+      eleMessage.focus();
+
       if (e) {
         console.log(e);
         return;
@@ -30,11 +40,12 @@ document.getElementById('fChat').addEventListener('submit', e => {
   );
 });
 
-document.getElementById('btnShareLocation').addEventListener('click', e => {
+eleSendLocation.addEventListener('click', e => {
   if (!navigator.geolocation) {
     alert('Geolocation is not supported by your browser');
     return;
   }
+  eleSendLocation.disabled = true;
 
   navigator.geolocation.getCurrentPosition(position => {
     socket.emit(
@@ -43,7 +54,11 @@ document.getElementById('btnShareLocation').addEventListener('click', e => {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude
       },
-      () => console.log('The message was delivered!')
+      () => {
+        eleSendLocation.disabled = false;
+
+        console.log('The message was delivered!');
+      }
     );
   });
 });
